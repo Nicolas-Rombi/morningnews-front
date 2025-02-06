91500a861ebb726fe5904dba1781eccca3dc4447
Nicolas-Rombi <nico461@hotmail.fr>
2025-02-06 20:31:32 +0100

    vercel

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { hideArticle, showAllArticles } from '../reducers/hiddenArticles';
+import { useSelector } from 'react-redux';
 import Head from 'next/head';
 import Article from './Article';
 import TopArticle from './TopArticle';
 import styles from '../styles/Home.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
-  const dispatch = useDispatch();
   const bookmarks = useSelector((state) => state.bookmarks.value);
   const hiddenArticles = useSelector((state) => state.hiddenArticles.value);
 
@@ -17,52 +13,36 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch('https://morningnews-back-five.vercel.app/articles')
+    fetch('http://localhost:3000/articles')
       .then(response => response.json())
       .then(data => {
         setTopArticle(data.articles[0]);
-        setArticlesData(data.articles.filter((_, i) => i > 0));
+        setArticlesData(data.articles.filter((data, i) => i > 0));
       });
   }, []);
 
-  const handleHideArticle = (articleId) => {
-    dispatch(hideArticle(articleId));
-  };
+  const filteredArticles = articlesData.filter((data) => !hiddenArticles.includes(data.title));
+  const articles = filteredArticles.map((data, i) => {
+    const isBookmarked = bookmarks.some(bookmark => bookmark.title === data.title);
+    return <Article key={i} {...data} isBookmarked={isBookmarked} />;
+  });
 
-  const handleShowAllArticles = () => {
-    dispatch(showAllArticles());
-  };
-
-  const articles = articlesData
-    .filter(article => !hiddenArticles.includes(article.id)) // Filtrer les articles masqués
-    .map((data, i) => {
-      const isBookmarked = bookmarks.some(bookmark => bookmark.title === data.title);
-      return (
-        <div key={i} className={styles.articleWrapper}>
-          <Article {...data} isBookmarked={isBookmarked} />
-          <FontAwesomeIcon 
-            icon={faEyeSlash} 
-            onClick={() => handleHideArticle(data.id)} 
-            className={styles.hideIcon} 
-          />
-        </div>
-      );
-    });
+  let topArticles;
+  if (bookmarks.some(bookmark => bookmark.title === topArticle.title)) {
+    topArticles = <TopArticle {...topArticle} isBookmarked={true} />
+  } else {
+    topArticles = <TopArticle {...topArticle} isBookmarked={false} />
+  }
 
   return (
     <div>
       <Head>
         <title>Morning News - Home</title>
       </Head>
-      {topArticle && <TopArticle {...topArticle} />}
+      {topArticles}
       <div className={styles.articlesContainer}>
         {articles}
       </div>
-      <FontAwesomeIcon 
-        icon={faEye} 
-        onClick={handleShowAllArticles} 
-        className={styles.showHiddenIcon} 
-      />
     </div>
   );
 }
